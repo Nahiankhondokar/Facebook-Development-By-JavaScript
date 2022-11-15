ba302ff7b4dc4d23652dbd3080c30b46735a923a
Nahiankhondokar <nahiankhondokarphp@gmail.com>
2022-11-15 18:10:56 +0600

    fb dev backend part 02 (user schema done)

--- api/models/User.js
+++ api/models/User.js
@@ -3,22 +3,38 @@ import mongoose from "mongoose";
 
 // student shcema design 
 const userSchema = mongoose.Schema({
-    name : {
+    first_name : {
         type : String,
-        required : [true, 'name feild is required'],
-        unique : true, 
+        required : true,
+        trim : true
+    }, 
+    sur_name : {
+        type : String,
+        required : true,
+        trim : true
+    }, 
+    username : {
+        type : String,
+        trim : true
+    },
+    secondary_name : {
+        type : String,
+        required : true,
         trim : true
     }, 
     email : {
         type : String,
-        required : [true, 'email feild is required'],
+        unique : true,
+        trim : true
+    }, 
+    mobile : {
+        type : String,
         unique : true, 
         trim : true
     }, 
-    cell : {
+    password : {
         type : String,
-        required : [true, 'cell feild is required'],
-        unique : true, 
+        required : true,
         trim : true
     }, 
     age : {
@@ -27,21 +43,81 @@ const userSchema = mongoose.Schema({
         trim : true
     },
     gender : {
+        type : String,
+        enum : ['Male', 'Female', 'Custom']
+    },
+    birth_date : {
+        type : String
+    }, 
+    profile_photo : {
+        type : String,
+        default : null
+    }, 
+    cover_photo : {
+        type : String,
+        default : null
+    }, 
+    work : {
+        type : Array,
+        default : []
+    },
+    edu : {
+        type : Array,
+        default : []
+    },
+    cover_photo : {
+        type : String,
+        default : null
+    },
+    living : {
+        type : String,
+        default : null
+    },
+    home_town : {
+        type : String,
+        default : null
+    },
+    relationship : {
+        type : String,
+        enum : ['Married', 'Single', 'In a Relationship']
+    },
+    joined : {
+        type : Date 
+    },
+    social : {
+        type : Array,
+        default : [] 
+    },
+    friends : {
+        type : Array,
+        default : [] 
+    },
+    follwing : {
+        type : Array,
+        default : [] 
+    },
+    follwers : {
+        type : Array,
+        default : [] 
+    },
+    request : {
+        type : Array,
+        default : [] 
+    },
+    posts : {
+        type : Array,
+        default : [] 
+    },
+    block : {
+        type : Array,
+        default : [] 
+    },
+    access_token : {
         type : String
     },
-    username : {
-        type : String,
-        required : [true, 'username feild is required'],
-        trim : true
-    }, 
-    password : {
-        type : String,
-        required : [true, 'password feild is required'], 
-        trim : true
-    }, 
-    photo : {
-        type : String,
-        default : 'avatar.png'
+    isActivate : {
+        type : Boolean,
+        default : false
     }, 
     isAdmin : {
         type : Boolean,
